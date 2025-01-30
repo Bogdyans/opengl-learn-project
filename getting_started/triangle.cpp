78c7cf3a178d03c4a5f355f6d7c0c706d23a5ec5
Bogdyans <BOGDYA2004@gmail.com>
2025-01-30 20:05:25 +0300

    feat(triangles): finish

--- getting_started/triangle.cpp
+++ getting_started/triangle.cpp
@@ -7,10 +7,15 @@
 
 const int SCR_WIDTH = 800;
 const int SCR_HEIGHT = 600;
-float vertices[] ={
-        -0.5f, -0.5f, 0.0f,
-         0.2f, 0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f
+float vertices[] = {
+        0.6f,  0.7f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+};
+unsigned int indices[] = {
+        3, 0, 2,
+        2, 0, 1
 };
 const char* vertex_shader_path = "../shaders/vertex.vert";
 const char* fragment_shader_path = "../shaders/fragment.frag";
@@ -133,17 +138,26 @@ int runT()
 
     unsigned int VBO;
     glGenBuffers( 1, &VBO );
+
     unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
+    glGenVertexArrays (1, &VAO );
+
+    unsigned int EBO;
+    glGenBuffers( 1, &EBO );
 
     glBindVertexArray(VAO);
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
     glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr );
     glEnableVertexAttribArray( 0 );
 
     glBindVertexArray( 0 );
+
+    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 
     while ( !glfwWindowShouldClose(window) )
     {
@@ -152,7 +166,8 @@ int runT()
 
         glUseProgram( shaderProgram );
         glBindVertexArray( VAO );
-        glDrawArrays( GL_TRIANGLES, 0, 3 );
+        //glDrawArrays( GL_TRIANGLES, 0, 3 );
+        glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -160,6 +175,8 @@ int runT()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
     glDeleteProgram(shaderProgram);
 
     return 0;
