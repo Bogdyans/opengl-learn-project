16e523e09a203e26c1c7d8b554dc166124bceb6e
Bogdyans <BOGDYA2004@gmail.com>
2025-02-01 19:28:53 +0300

    lesson(shaaders): shader class add and rewrite

--- shared/shader.h
+++
@@ -1,7 +0,0 @@
-#ifndef OPENGL_LEARN_SHADER_H
-#define OPENGL_LEARN_SHADER_H
-
-unsigned int createShader( const char* shaderSource, int shaderType );
-bool checkProgramLinkingSuccess( unsigned int program );
-
-#endif //OPENGL_LEARN_SHADER_H
